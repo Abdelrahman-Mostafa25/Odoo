7fbca4f54a9e9157b6fc64a0df83f403d4ad2c65
Abdelrahman-Mostafa25 <88713984+Abdelrahman-Mostafa25@users.noreply.github.com>
2021-08-10 13:07:00 +0200

    first commit

---
+++ it_courses/it_courses/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "it_courses",
+
+    'summary': """
+        This is a new module for store data about courses for all 
+        pepole""",
+
+    'description': """
+       Courses for every one 
+    """,
+
+    'author': "Abdo Mostafa",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/it_coursesviews.xml',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
